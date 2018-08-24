e851c5a6fa6d5ebedb43f133ba115e519e242155
uttamchauhan <uttamchauhan@Uttam3148783.Nagarro.local>
2018-08-24 10:50:49 +0530

    adding test case for main

---
+++ test/com/assessment/retaildiscount/MainTest.java
@@ -0,0 +1,14 @@
+package com.assessment.retaildiscount;
+
+import org.junit.Test;
+
+import com.assessment.retaildiscount.main.Main;
+
+public class MainTest {
+
+	@Test
+	public void testApp() {
+		String[] args= {};
+		Main.main(args);
+	}
+}
